ef4c9ba158334b171d4e862e35cf176a53cfc535
valeriankass <lady.komarnickaya@gmail.com>
2020-03-02 15:35:57 +0100

    Functionality update #1. Functions constructor, addRoomToList, decreasePuzzlesLeftToSolve, isCompleted, getTextAboutLevel are updated. The version is waiting for Ece's, Matthew's and Taylor's comments. Currently, there are 3 warnings, cannot resolve them without firstly getting your review.

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -5,39 +5,33 @@ public class Level {
 
     private static List<Room> listOfRooms = new ArrayList<>();
     private static int puzzlesLeftToSolve;
-
-    /*
-    public static void constructor(int amountOfPuzzlesToSolve) {
-        while (amountOfPuzzlesToSolve > 0) {            //while amount of puzzles to solve > 0 -> continue on level
-                                                        //otherwise isCompleted()
-            amountOfPuzzlesToSolve = puzzlesLeftToSolve;
-                                                        //Text about level - will Be about info when player enters.
-                                                        // Mario levels: basic level
-                                                        //Underground - 2nd level. Story text.
-                                                        //Story text : bonus story creator
-        }
+    private int amountOfPuzzlesLeftToSolve;
+    private String nameOfTheFloor = "basic"; //not sure if this is the way to go for basic implementation.
+    private String storyText;
+    //Constructor
+    public void constructor(int amountOfPuzzlesLeftToSolve,
+                            String storyText) {
+        storyText = String.format("You are on the %s level.", nameOfTheFloor);
+        this.amountOfPuzzlesLeftToSolve = puzzlesLeftToSolve;
+        this.storyText = storyText;
     }
-    */
 
-    public static void addRoomToList() {
-                                            //how do we do that? - within the parameter, you get room and
-                                            //it has to be added to list of rooms
-                                            //List.add(room)
-
+    public void addRoomToList(Room roomToAdd) {
+        listOfRooms.add(roomToAdd);
     }
-    public static void decreasePuzzlesLeftToSolve() {
-                                            //if number of puzzles left to solve at the end of the puzzle is
-                                            // smaller than amount of puzzle to solve before solved puzzle
+
+    public void decreasePuzzlesLeftToSolve() {
         puzzlesLeftToSolve -= 1;
     }
-    public static boolean isCompleted() {
+    public boolean isCompleted() {
         return puzzlesLeftToSolve == 0;
     }
-    public static String getTextAboutLevel() {
-        return "blablabla";
+    public String getTextAboutLevel() {
+        return storyText;
+    }
+    //tmp
+    public String print() {
+        return getTextAboutLevel();
     }
 
-    //Can be deleted later public : static String print() {
-        //return String.format(getTextAboutLevel());
-    //}
 }
