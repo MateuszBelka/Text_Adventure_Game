66d6f2cc0561240e2b10b133eb00d29c743130dd
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-02 10:30:26 +0100

    "set up skeleton of Initialisation class"

--- src/main/java/Initialisation.java
+++ src/main/java/Initialisation.java
@@ -1,3 +1,14 @@
 public class Initialisation {
-    public void print() {System.out.println("Initialisation");} //delete this (also in main) when populating this class
+    //Variable Declaration
+    //Constructor
+    //Get Methods
+    //Set Methods
+    //Main Methods
+    /*
+     * one public method for initializing whole game and from there you call private methods that call appropriate constructors
+     */
+    //Temporary Methods
+    public void print() {
+        System.out.println("Initialisation");
+    } //delete this (also in main) when populating this class
 }
