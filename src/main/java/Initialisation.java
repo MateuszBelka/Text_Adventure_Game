e9fd002ee12bd3f510bbe0f71f18e939a94ad126
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-03 13:37:58 +0100

    set up init classes

--- src/main/java/Initialisation.java
+++ src/main/java/Initialisation.java
@@ -1,45 +1,179 @@
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.ArrayList;
+
 public class Initialisation {
+    //Variable Declaration
+    ArrayList<Game> games;
+    ArrayList<Player> players;
+    ArrayList<InputHandler> inputHandlers;
+    ArrayList<Output> outputs;
+    //ArrayList<Inventory> inventories;
+    ArrayList<Level> levels;
+    ArrayList<Room> rooms;
+    ArrayList<StaticItem> staticItems;
+    ArrayList<PickupableItem> pickupableItems;
+    Boolean exitSwitch;
+
+    public Initialisation() {
+        this.exitSwitch = false;
+    }
+
+    //Get Methods
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+    public ArrayList<InputHandler> getInputHandlers() {
+        return inputHandlers;
+    }
+    public ArrayList<Output> getOutputs() {
+        return outputs;
+    }
+    /*
+    public ArrayList<Inventory> getInventories() {
+
+    }
+    */
+    public ArrayList<Level> getLevels() {
+        return levels;
+    }
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+    public ArrayList<StaticItem> getStaticItems() {
+        return staticItems;
+    }
+    public ArrayList<PickupableItem> getPickupableItems() {
+        return pickupableItems;
+    }
+    public Boolean getExitSwitch() {
+        return exitSwitch;
+    }
+    //Set Methods
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+    public void setInputHandlers(ArrayList<InputHandler> inputHandlers) {
+        this.inputHandlers = inputHandlers;
+    }
+    public void setOutputs(ArrayList<Output> outputs) {
+        this.outputs = outputs;
+    }
+    public void setLevels(ArrayList<Level> levels) {
+        this.levels = levels;
+    }
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+    public void setStaticItems(ArrayList<StaticItem> staticItems) {
+        this.staticItems = staticItems;
+    }
+    public void setPickupableItems(ArrayList<PickupableItem> pickupableItems) {
+        this.pickupableItems = pickupableItems;
+    }
+    public void setExitSwitch(Boolean exitSwitch) {
+        this.exitSwitch = exitSwitch;
+    }
     /* The following function is responsible for starting the entire game
      * and as such overlooks class specific initialization functions.
      */
-    public void initializeStorySpecificData() {
+    public void initializeEntireStory() {
+        //initialise classes
+        setGames(initializeGames());
+        setPlayers(initializePlayers());
+        setInputHandlers(initializeInputHandlers());
+        setOutputs(initializeOutputs());
+        //setInventories(initializeInventories());
+        setLevels(initializeLevels());
+        setRooms(initializeRooms());
+        setStaticItems(initializeStaticItems());
+        setPickupableItems(initializePickupableItems());
 
+        //configure classes
     }
 
-    private void initializeGame() {
+    private ArrayList<Game> initializeGames() {
+        ArrayList<Game> games = new ArrayList<>();
+        Game game = new Game();
 
+        games.add(game);
+
+        return games;
     }
 
-    private void initializePlayer() {
+    private ArrayList<Player> initializePlayers() {
+        ArrayList<Player> players = new ArrayList<>();
+        Player player = new Player();
 
+        players.add(player);
+
+        return players;
     }
 
-    private void initializeInputHandler() {
+    private ArrayList<InputHandler> initializeInputHandlers() {
+        ArrayList<InputHandler> inputHandlers = new ArrayList<>();
+        InputHandler inputHandler = new InputHandler();
 
+        inputHandlers.add(inputHandler);
+
+        return inputHandlers;
     }
 
-    private void initializeStoryTextOutput() {
+    private ArrayList<Output> initializeOutputs() {
+        ArrayList<Output> outputs = new ArrayList<>();
+        Output output = new Output();
 
+        outputs.add(output);
+
+        return outputs;
     }
 
-    private void initializeInventory() {
+    /*
+    private ArrayList<Inventory> initializeInventories() {
 
     }
+    */
 
-    private void initializeLevel() {
+    private ArrayList<Level> initializeLevels() {
+        ArrayList<Level> levels = new ArrayList<>();
+        Level level = new Level();
 
+        levels.add(level);
+
+        return levels;
     }
 
-    private void initializeRoom() {
+    private ArrayList<Room> initializeRooms() {
+        ArrayList<Room> rooms = new ArrayList<>();
+        Room room = new Room();
 
+        rooms.add(room);
+
+        return rooms;
     }
 
-    private void initializeStaticItem() {
+    private ArrayList<StaticItem> initializeStaticItems() {
+        ArrayList<StaticItem> staticItems = new ArrayList<>();
+        StaticItem staticItem = new StaticItem();
 
+        staticItems.add(staticItem);
+
+        return staticItems;
     }
 
-    private void initializePickupableItem() {
+    private ArrayList<PickupableItem> initializePickupableItems() {
+        ArrayList<PickupableItem> pickupableItems = new ArrayList<>();
+        PickupableItem pickupableItem = new PickupableItem();
 
+        pickupableItems.add(pickupableItem);
+
+        return pickupableItems;
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Initialisation init = new Initialisation();
 
         //main game loop
-        while (!game.isCompleted() && exitSwitch.equals(false)) {
+        while (!init.getGames().get(0).isCompleted() && init.getExitSwitch().equals(false)) {
             String input = scanner.nextLine();
-            if (input.equals("exit")) {exitSwitch = true;}
-            inputHandler.handleInput(input, player.getCurrentRoom());
+            if (input.equals("exit")) {init.setExitSwitch(true);}
+            init.getInputHandlers().get(0).handleInput(input, init.getPlayers().get(0));
         }
     }
 }
