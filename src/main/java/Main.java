55dda82f8b0096851a1077f67b1a7e625ef5ecea
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-06 22:15:52 +0100

    fix input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
+import org.beryx.textio.TextIO;
 import org.beryx.textio.TextTerminal;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Initialisation init = new Initialisation();
         init.initializeEntireSystem();
 
@@ -12,22 +12,22 @@ public class Main {
         Player player = game.getPlayer();
         InputValidator inputValidator = init.getInputValidators().get(0);
         StoryTextOutput storyTextOutput = init.getStoryTextOutputs().get(0);
+        TextIO textIO = init.getTextIO();
         TextTerminal terminal = init.getTerminal();
-
-        terminal.printf("(GAME DEMO) (tip: you can type 'exit' at any time)\n\n");
 
         //main game loop
         while (!game.isCompleted() && init.getExitSwitch().equals(false)) {
-//            System.out.println("******StoryText:******");
+            /* DEBUG MODE
+             * System.out.println("******INFO FOR TESTING:******");
+             * System.out.println("current room: " + player.getCurrentRoom());
+             * System.out.println("current room directions: " + player.getCurrentRoom().getListOfAvailableDirections());
+             * System.out.println("current room pickupable items: " + player.getCurrentRoom().getListOfPickupableItems());
+             * System.out.println("current room static items: " + player.getCurrentRoom().getListOfStaticItems());
+             * System.out.println("player inventory: " + player.getInventory().getItemsInInventory());
+             * System.out.println("******StoryText:******");
+             */
             storyTextOutput.printStoryText(player, terminal);
-//            System.out.println("******INFO FOR TESTING:******");
-//            System.out.println("current room: " + player.getCurrentRoom());
-//            System.out.println("current room directions: " + player.getCurrentRoom().getListOfAvailableDirections());
-//            System.out.println("current room pickupable items: " + player.getCurrentRoom().getListOfPickupableItems());
-//            System.out.println("current room static items: " + player.getCurrentRoom().getListOfStaticItems());
-//            System.out.println("player inventory: " + player.getInventory().getItemsInInventory());
-            String input = scanner.nextLine();
-            if (input.equals("exit")) {init.setExitSwitch(true);}
+            String input = textIO.newStringInputReader().read("");
             inputValidator.validateInput(input, player, terminal);
         }
     }
