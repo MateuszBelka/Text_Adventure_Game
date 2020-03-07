fdab49dc3ffde323c53cce9dacebfe5cb7715c91
Mateusz Belka <46693948+MateuszBelka@users.noreply.github.com>
2020-03-08 00:01:33 +0100

    Revert "Assignment 2 - Team 3 - Text Adventure Game"

---
+++ classdiagram/README.md
@@ -0,0 +1,1 @@
+This folder is for our collaborative class diagram.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,5 @@
-import org.beryx.textio.TextIO;
-import org.beryx.textio.TextTerminal;
-
-import java.util.Scanner;
-
 public class Main {
-    public static void main (String[] args) {
-        Initialisation init = new Initialisation();
-        init.initializeEntireSystem();
-
-        Game game = init.getGames().get(0);
-        Player player = game.getPlayer();
-        InputValidator inputValidator = init.getInputValidators().get(0);
-        StoryTextOutput storyTextOutput = init.getStoryTextOutputs().get(0);
-        TextIO textIO = init.getTextIO();
-        TextTerminal terminal = init.getTerminal();
-
-        terminal.printf("(GAME DEMO) (tip: you can type 'exit' at any time)\n");
-        //main game loop
-        while (!game.isCompleted() && init.getExitSwitch().equals(false)) {
-            /* DEBUG MODE
-             * System.out.println("******INFO FOR TESTING:******");
-             * System.out.println("current room: " + player.getCurrentRoom());
-             * System.out.println("current room directions: " + player.getCurrentRoom().getListOfAvailableDirections());
-             * System.out.println("current room pickupable items: " + player.getCurrentRoom().getListOfPickupableItems());
-             * System.out.println("current room static items: " + player.getCurrentRoom().getListOfStaticItems());
-             * System.out.println("player inventory: " + player.getInventory().getItemsInInventory());
-             * System.out.println("******StoryText:******");
-             */
-            storyTextOutput.printStoryText(player, terminal);
-            String input = textIO.newStringInputReader().read("");
-            if (input.equals("exit")) { init.setExitSwitch(true); }
-            inputValidator.validateInput(input, player, terminal);
-        }
-        terminal.abort();
+    public static void main (String[] args){
+        System.out.println("Welcome to Software Design");
     }
 }
