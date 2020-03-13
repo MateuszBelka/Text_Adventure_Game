7d1eb3479d0dac4c11fbfd744e5639b5c6da1ec9
valeriankass <lady.komarnickaya@gmail.com>
2020-03-13 17:13:14 +0100

    Comments on level and pickupable item

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -8,20 +8,27 @@ public class Level {
     private Boolean enterLevel = true;
 
     //Get Methods
+    //returns the list of the rooms on the level
     public ArrayList<Room> getListOfRooms() { return listOfRooms; }
+    //returns an integer of puzzles left to solve on the level
     public int getPuzzlesLeftToSolve() { return puzzlesLeftToSolve; }
+    //returns a description about the level
     public String getTextAboutLevel() { return storyText; }
 
     //Set Methods
+    //setting the list of rooms on the level
     public void setListOfRooms(ArrayList<Room> listOfRooms) { this.listOfRooms = listOfRooms; }
-    public void updatePuzzlesLeftToSolve() { //puzzles left to solve in the room
+    //sets number of puzzles left to solve depending on the number of static items per room
+    public void updatePuzzlesLeftToSolve() {
         puzzlesLeftToSolve = 0;
         for (Room room : getListOfRooms()) {
             puzzlesLeftToSolve += room.getListOfStaticItems().size();
         }
     }
+    //setting the story description of a level
     public void setStoryText(String storyText) {this.storyText = storyText; }
-    public boolean switchEnterLevel(){ //entering a new level
+    //setting a boolean variable enterLevel to true when a new level is entered
+    public boolean switchEnterLevel(){
         if (enterLevel.equals(true)){
             enterLevel = false;
             return true;
@@ -30,7 +37,10 @@ public class Level {
     }
 
     //Main Methods
+    //A method used for game-creator mode when a creator has an option to add a room to his level
     public void addRoomToList(Room roomToAdd) { listOfRooms.add(roomToAdd); }
+    //decrementing the number of puzzles left to solve
     public void decreasePuzzlesLeftToSolve() { puzzlesLeftToSolve -= 1; }
+    //returns true when all puzzles are solved
     public boolean isCompleted() { return puzzlesLeftToSolve == 0; }
 }
