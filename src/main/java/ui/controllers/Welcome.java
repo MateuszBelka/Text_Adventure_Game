8e97ed2613dbd471382d03777db0aad739ca32ab
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-16 11:54:54 +0100

    rewrite exception throwing

--- src/main/java/ui/controllers/Welcome.java
+++ src/main/java/ui/controllers/Welcome.java
@@ -12,28 +12,20 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Welcome {
-    public void newGameButtonClicked(ActionEvent actionEvent) {
-        try {
-            URL newGameURL = new File("src/main/java/UI/scenes/newGame.fxml").toURI().toURL();
-            Parent newGameParent = FXMLLoader.load(newGameURL);
-            Scene newGameScene = new Scene (newGameParent, 960, 720);
-            Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-            primaryStage.setScene(newGameScene);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void newGameButtonClicked(ActionEvent actionEvent) throws IOException {
+        URL newGameURL = new File("src/main/java/UI/scenes/newGame.fxml").toURI().toURL();
+        Parent newGameParent = FXMLLoader.load(newGameURL);
+        Scene newGameScene = new Scene (newGameParent, 960, 720);
+        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setScene(newGameScene);
     }
 
-    public void loadGameButtonClicked(ActionEvent actionEvent) {
-        try {
-            URL loadGameURL = new File("src/main/java/UI/scenes/loadGame.fxml").toURI().toURL();
-            Parent loadGameParent = FXMLLoader.load(loadGameURL);
-            Scene loadGameScene = new Scene (loadGameParent, 960, 720);
-            Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-            primaryStage.setScene(loadGameScene);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void loadGameButtonClicked(ActionEvent actionEvent) throws IOException {
+        URL loadGameURL = new File("src/main/java/UI/scenes/loadGame.fxml").toURI().toURL();
+        Parent loadGameParent = FXMLLoader.load(loadGameURL);
+        Scene loadGameScene = new Scene (loadGameParent, 960, 720);
+        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setScene(loadGameScene);
     }
 
     public void quitButtonClicked(ActionEvent actionEvent) {
