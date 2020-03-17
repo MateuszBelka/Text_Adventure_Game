d38d4188a21ed7f58b2f369119d47ebe11e02877
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-17 02:02:22 +0100

    remove old lib

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,3 @@
-import org.beryx.textio.TextTerminal;
-import org.beryx.textio.swing.SwingTextTerminal;
 import java.util.ArrayList;
 
 public class Player {
@@ -57,14 +55,14 @@ public class Player {
 
     public Inventory getInventory() { return inventory; }
 
-    public void progressPlayer (Initialisation init, TextTerminal<SwingTextTerminal> terminal){
+    public void progressPlayer (Initialisation init){
         ArrayList<Level> levels = init.getLevels();
-        if (levels.size() == 1){ terminal.printf("Game Completed!\n"); }
+        if (levels.size() == 1){ System.out.println("Game Completed!\n"); }
         else {
             currentLevel = levels.get(1);
             currentRoom = currentLevel.getListOfRooms().get(0);
             currentLevel.updatePuzzlesLeftToSolve();
-            terminal.printf("Level Completed!\n");
+            System.out.println("Level Completed!\n");
             init.deletePreviousLevel();
         }
     }
