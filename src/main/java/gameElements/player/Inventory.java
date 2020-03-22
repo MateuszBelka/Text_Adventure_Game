f66a9992eb3baf23a9eec7dceb8f8e50041af9c6
valeriankass <lady.komarnickaya@gmail.com>
2020-03-22 04:12:17 +0100

    Inventory functionality is updated in accordance with a new layout + command inventory handler(temp version)

--- src/main/java/gameElements/player/Inventory.java
+++ src/main/java/gameElements/player/Inventory.java
@@ -1,4 +1,33 @@
 package gameElements.player;
 
+import gameElements.levelAndContents.Item;
+
+import java.util.ArrayList;
+
 public class Inventory {
+
+    private final ArrayList<Item> listOfPickupableItems = new ArrayList<>();
+
+    //Get Methods
+    public ArrayList<Item> getItemsInInventory() {return listOfPickupableItems;}
+    public Item getItemByName (String name){ //retrieving item by name
+        if (getItemsInInventory() != null){
+            for (int i = 0; i < getItemsInInventory().size(); i++){
+                if (name.equals(getItemsInInventory().get(i).getName())){
+                    return getItemsInInventory().get(i);
+                }
+            }
+        }
+        return null;
+    }
+
+    //Set Methods
+    public void addToInventory(Item item){ getItemsInInventory().add(item);}
+
+    public void deleteItemFromInventory(Item item){ getItemsInInventory().remove(item);}
+
+    public boolean canBePickedUp(Item item) { return listOfPickupableItems.contains(item); }
+
+    public void inventoryCommand() { System.out.println(getItemsInInventory()); }
+
 }
