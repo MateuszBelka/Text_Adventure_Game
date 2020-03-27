5299ae54f4eb1f64970891a18966a3f1717de3c3
valeriankass <lady.komarnickaya@gmail.com>
2020-03-27 20:56:45 +0100

    Updated functionality and organization of functions in playerStats, PlayerLevelling and PlayerHunger. Todo: 2 lines of code: printing a warning message for a user when the hunger is low

--- src/main/java/gameElements/player/PlayerHungerProgression.java
+++ src/main/java/gameElements/player/PlayerHungerProgression.java
@@ -1,4 +1,33 @@
 package gameElements.player;
 
 public class PlayerHungerProgression {
+
+    private transient PlayerStats playerStats;
+
+    public PlayerStats getPlayerStats() {
+        return playerStats;
+    }
+
+    public void setPlayerStats(PlayerStats playerStats) {
+        this.playerStats = playerStats;
+    }
+
+    //called when a player did some action, for example move, action, etc. Exception: consumption
+    protected void decreaseCurrentHunger() { playerStats.setCurrentHunger(playerStats.currentHunger - 10); }
+
+    //called when a character consumed something
+    protected void increaseCurrentHunger() { playerStats.setCurrentHunger(playerStats.currentHunger + 15); }
+
+    //the type should be changed depending on implementation, probably boolean or string, not sure yet
+    public void checkCurrentHunger() {
+        if (playerStats.getCurrentHunger() <= 10 && playerStats.getCurrentHunger() > 0) {
+            //return additional message that will warn a player character being close to starving
+        }
+        else if (playerStats.getCurrentHunger() == 0) {
+            playerStats.setCurrentHealth(playerStats.currentHealth - 20);
+            //return warning message that a character is starving and starts to lose health
+        }
+    }
+
+
 }
