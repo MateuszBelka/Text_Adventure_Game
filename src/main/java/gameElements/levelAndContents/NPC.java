59d948cba38a2a5f42034985dd7b1f9fd97aee7c
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-27 14:24:29 +0100

    fix entity moved vars

--- src/main/java/gameElements/levelAndContents/NPC.java
+++ src/main/java/gameElements/levelAndContents/NPC.java
@@ -5,6 +5,22 @@ import initialisation.InitOfClassesThroughSaveFile;
 import java.util.ArrayList;
 
 public class NPC extends Entity {
+    private int damage;
+    private int currentHealth;
+
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
     public static NPC getNPCByName (String name){
         ArrayList<NPC> listOfNpcsOnLocation = InitOfClassesThroughSaveFile.getPlayerStats().getCurrentLocation().getListOfNPCs();
         NPC searchedNPC = null;
