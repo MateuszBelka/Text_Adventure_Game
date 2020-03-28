f5e53b2d6ac7ef376fd8c68d6b61b959a4be643e
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-28 06:11:37 +0100

    combat input

--- src/main/java/input/commands/DoAttack.java
+++ src/main/java/input/commands/DoAttack.java
@@ -3,12 +3,15 @@ package input.commands;
 import gameElements.battle.BattleSequence;
 import gameElements.levelAndContents.Item;
 import gameElements.levelAndContents.NPC;
+import input.combatValidation.CombatValidation;
 
 public class DoAttack {
     public static void doAttack(Item item){
     }
 
     public static void doAttack(NPC npc){
+        BattleSequence.setCurrentEnemy(npc);
+        CombatValidation.validator("attack");
     }
 
     public static void doAttack(){
