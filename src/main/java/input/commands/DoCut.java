5c7a03d1d8ee4b96d48fabd3b7c62e3b7f56d9c1
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-29 11:17:01 +0200

    remove double null checks

--- src/main/java/input/commands/DoCut.java
+++ src/main/java/input/commands/DoCut.java
@@ -6,17 +6,13 @@ import util.ExistenceCheck;
 
 public class DoCut {
     public static void doCutWith(Item itemToBeCut, Item itemToCutWith) {
-        if (!ExistenceCheck.doesItExist(itemToBeCut, itemToCutWith, "The item you are trying to consume doesn't exist.")) return;
-
     }
 
     public static void doCutWith(NPC npcToBeCut, Item itemToCutWith) {
-        if (!ExistenceCheck.doesItExist(npcToBeCut, npcToBeCut, "The item you are trying to consume doesn't exist.")) return;
 
     }
 
     public static void doCutItemWithItem(Item itemToCut, Item itemToCutWith) {
-        if (!ExistenceCheck.doesItExist(itemToCut, itemToCutWith, "The item you are trying to consume doesn't exist.")) return;
 
     }
 
