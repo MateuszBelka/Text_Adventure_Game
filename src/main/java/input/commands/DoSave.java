65905d543f3c54502d385dffc2562a0e73d49341
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-03-29 13:59:02 +0200

    saving fully works

--- src/main/java/input/commands/DoSave.java
+++ src/main/java/input/commands/DoSave.java
@@ -1,9 +1,18 @@
 package input.commands;
 
 import output.NonStoryPrinter;
+import saveAndLoad.Save;
+
+import java.io.File;
+import java.net.URISyntaxException;
 
 public class DoSave {
-    public static void doSave() {
+    public static void doSave() throws URISyntaxException {
+        String path = new File(DoSave.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+        path = path.substring(0, path.length() - 32);
+        path += "resources\\saves\\save0.json";
+
+        Save.writeGameSave(path);
         NonStoryPrinter.print("Game has been successfully saved!");
     }
 }
