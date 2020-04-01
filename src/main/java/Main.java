c8a313cf43dbad08ea083d3422804835bc1a591a
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-04-01 10:58:04 +0200

    remove code for creating json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,6 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException {
-        CreateDemo.createDemo();
-        CreateGame.createGame();
         /* DEV NOTE:
          * Keep in mind that nothing after this line of code will be executed.
          * If you'd like some code to run you will have to do that through
